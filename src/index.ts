3474c171e2775577c3e27410aecdba3d943e32a0
lunchspider <lunchspider03@gmail.com>
2023-10-09 23:31:39 +0530

    intial commit done!

--- src/index.ts
+++ src/index.ts
@@ -48,10 +48,24 @@ async function fillCardInfo(page: Page, record: GoogleSpreadsheetRow) {
     });
 }
 
+async function clearCart(page: Page) {
+    await page.goto('https://store.mi.com/in/cart/');
+    await page.waitForSelector('.cart__main', { visible: true });
+    await new Promise((r) => setTimeout(r, 1000));
+    let deleteButtons = await page.$('button.cart-item__delete');
+    while (deleteButtons !== null) {
+        await deleteButtons.click();
+        await page.waitForSelector('div[aria-label = "Modal"] footer div:nth-child(2)');
+        await page.click('div[aria-label = "Modal"] footer div:nth-child(2)');
+        await new Promise((r) => setTimeout(r, 1000));
+        deleteButtons = await page.$('button.cart-item__delete');
+    }
+}
+
 
 async function order(page: Page, record: GoogleSpreadsheetRow) {
     await login(page, record);
-    await wait();
+    await clearCart(page);
     await page.goto(record.get("LINK"));
     await page.waitForSelector("#nav-buy_now");
     await page.click("#nav-buy_now");
